597f7d909cdfa1449f6f8421aa4471dff8e2a4e6
Emily Monte Calvo <emilymontecalvo@gmail.com>
2016-10-27 19:24:03 +0000

    initial commit - less, webpack, babel

---
+++ js/index.js
@@ -0,0 +1,116 @@
+var $ = require('jquery');
+var makeBackgroundRed = require('./background');
+
+$(document).ready(function() {
+    makeBackgroundRed();
+});
+
+var QUESTIONS = [
+    {
+        text: 'question 1',
+        answers: [
+            '0815',
+            '2B',
+            'BAM128',
+            'Barely'
+        ],
+        correct: 0
+    },
+    {
+        text: 'answer question 2',
+        answers: [
+            '0815',
+            '2B',
+            'BAM128',
+            'Barely'
+        ],
+        correct: 1
+    },
+    {
+        text: 'what is the answer?',
+        answers: [
+            '0815',
+            '2B',
+            'BAM128',
+            'Barely'
+        ],
+        correct: 2
+    },
+    {
+        text: 'please answer',
+        answers: [
+            '0815',
+            '2B',
+            'BAM128',
+            'Barely'
+        ],
+        correct: 3
+    }
+];
+
+var questionsPageElement = $('.questions-page');
+var questionCurrentElement = $('.question-current');
+var questionsTotalElement = $('.questions-total');
+var questionElement = $('.question');
+var answersElement = $('.answers');
+
+var resultsPageElement = $('.results-page');
+var scoreElement = $('.score');
+var restartButtonElement = $('.restart-button');
+
+var showResults = function() {
+    questionsPageElement.hide();
+    resultsPageElement.show();
+};
+
+var showQuestions = function() {
+    resultsPageElement.hide();
+    questionsPageElement.show();
+};
+
+var resetScore = function() {
+    scoreElement.text(0);
+};
+
+var increaseScore = function() {
+    var score = parseInt(scoreElement.text(), 10);
+    scoreElement.text(score + 1);
+};
+
+var setQuestion = function(questionIndex) {
+    var question = QUESTIONS[questionIndex];
+    questionCurrentElement.text(questionIndex);
+    questionElement.text(question.text);
+    answersElement.empty();
+    for (var i=0; i<question.answers.length; i++) {
+        var answer = question.answers[i];
+        answersElement.append('<li><button type="button">' + answer + '</button></li>');
+    }
+};
+
+answersElement.on('click', 'button', function() {
+    var choice = $(this).parent().index();
+    var questionIndex = parseInt(questionCurrentElement.text(), 10);
+    var question = QUESTIONS[questionIndex];
+    if (question.correct === choice) {
+        increaseScore();
+    }
+
+    if (questionIndex + 1 < QUESTIONS.length) {
+        setQuestion(questionIndex + 1);
+    }
+    else {
+        showResults();
+    }
+});
+
+restartButtonElement.click(function() {
+    setQuestion(0);
+    resetScore();
+    showQuestions();
+});
+
+$(document).ready(function() {
+    questionsTotalElement.text(QUESTIONS.length);
+    setQuestion(0);
+});
